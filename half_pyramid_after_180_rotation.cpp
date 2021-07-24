66f14e45044de2850135c50d93f960646c02a77a
Vibhuti Patel <72439885+Vibhs-3401@users.noreply.github.com>
2021-07-24 16:03:13 +0530

    Update half_pyramid_after_180_rotation.cpp

--- half_pyramid_after_180_rotation.cpp
+++ half_pyramid_after_180_rotation.cpp
@@ -7,10 +7,29 @@ int main() {
   cin>>row;
   
   for (int i=1; i<=row; i++) {
-    for (int j = i; j>=1; j--) {
-        cout<<"*";
+    for (int j = 1; j<=row; j++) {
+        if (j <= row-i) cout<<" ";
+        else cout<<"*";
     }
     cout<<endl;
   }
   return 0;
 }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//   int row;
+//   cin>>row;
+  
+//   for (int i=1; i<=row; i++) {
+//     for (int j = i; j>=1; j--) {
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//   }
+//   return 0;
+// }
